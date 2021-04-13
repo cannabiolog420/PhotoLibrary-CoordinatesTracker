56c23d2a7d352ef32541048de81094d0108e498c
cannabiolog420 <darkstar125@mail.ru>
2021-04-14 01:46:37 +0300

    Initial commit

---
+++ AppCraftTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppCraftTest
+//
+//  Created by cannabiolog420 on 14.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
